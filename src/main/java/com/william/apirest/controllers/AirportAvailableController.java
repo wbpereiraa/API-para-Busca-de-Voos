721955f834328d3511c759964d59750a2a1a8e35
William Barbosa <wb.pereiraa@gmail.com>
2024-06-21 00:34:28 -0300

    Adicionado metodo GET com filtro por nome do aeroporto

--- src/main/java/com/william/apirest/controllers/AirportAvailableController.java
+++ src/main/java/com/william/apirest/controllers/AirportAvailableController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.william.apirest.dtos.AirportAvailableRecordDto;
@@ -22,33 +24,31 @@ import com.william.apirest.repositories.AirportAvailableRepository;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping(value = "/airports")
 public class AirportAvailableController {
 
 	@Autowired
 	AirportAvailableRepository airportAvailableRepository;
 	
-	@PostMapping("/airportavailable")
+	@PostMapping(value = "/airportavailable")
 	public ResponseEntity<AirportAvailable> saveAirport(@RequestBody @Valid AirportAvailableRecordDto airportAvailableRecordDto){
 		var airportAvailable = new AirportAvailable();
 		BeanUtils.copyProperties(airportAvailableRecordDto, airportAvailable);
 		return ResponseEntity.status(HttpStatus.CREATED).body(airportAvailableRepository.save(airportAvailable));
 	}
 	
-	@GetMapping("/airportavailable")
+	@GetMapping(value = "/airportavailable")
 	public ResponseEntity<List<AirportAvailable>> getAllAirports(){
 		return ResponseEntity.status(HttpStatus.OK).body(airportAvailableRepository.findAll());
 	}
 	
-	@GetMapping("/airportavailable/{id}")
-	public ResponseEntity<Object> getOneAirport(@PathVariable(value="id") String airportName){
-		Optional<AirportAvailable> airport = airportAvailableRepository.findById(airportName);
-		if(airport.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Airport not found.");
-		}
-		return ResponseEntity.status(HttpStatus.OK).body(airport.get());
+	
+	@GetMapping(value = "/filter")
+	public ResponseEntity<List<AirportAvailable>> findByAirport(@RequestParam("airportName")String airportName){
+		 return ResponseEntity.status(HttpStatus.OK).body(airportAvailableRepository.findByairportNameContaining(airportName));
 	}
 	
-	@PutMapping("/airportavailable/{id}")
+	@PutMapping(value = "/airportavailable/{id}")
 	public ResponseEntity<Object> updateAirport(@PathVariable(value="id") String airportName, @RequestBody @Valid AirportAvailableRecordDto airportAvailableRecordDto){
 		Optional<AirportAvailable> airport = airportAvailableRepository.findById(airportName);
 		if(airport.isEmpty()) {
@@ -59,7 +59,7 @@ public class AirportAvailableController {
 		return ResponseEntity.status(HttpStatus.OK).body(airportAvailableRepository.save(AirportAvailable));
 	}
 	
-	@DeleteMapping("/airportavailable/{id}")
+	@DeleteMapping(value = "/airportavailable/{id}")
 	public ResponseEntity<Object> deleteAirport(@PathVariable(value="id") String airportName){
 		Optional<AirportAvailable> airport = airportAvailableRepository.findById(airportName);
 		if(airport.isEmpty()) {
--- src/main/java/com/william/apirest/repositories/AirportAvailableRepository.java
+++ src/main/java/com/william/apirest/repositories/AirportAvailableRepository.java
@@ -1,11 +1,13 @@
 package com.william.apirest.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.william.apirest.entities.AirportAvailable;
 
-@Repository
+
 public interface AirportAvailableRepository extends JpaRepository <AirportAvailable, String>{
 
+	List<AirportAvailable> findByairportNameContaining(String airportName);
 }
