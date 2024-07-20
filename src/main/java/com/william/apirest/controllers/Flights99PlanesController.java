73ebd21cdf6069c372dd83aaa6c0b2ab1a318580
William Barbosa <wb.pereiraa@gmail.com>
2024-07-20 12:41:38 -0300

    Adicionando a camada de servico na classe Flights99Planes e refatorando a camada de controller

--- src/main/java/com/william/apirest/controllers/Flights99PlanesController.java
+++ src/main/java/com/william/apirest/controllers/Flights99PlanesController.java
@@ -1,10 +1,8 @@
 package com.william.apirest.controllers;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +19,7 @@ import com.william.apirest.dtos.Flights99PlanesPatchRecordDto;
 import com.william.apirest.dtos.Flights99PlanesRecordDto;
 import com.william.apirest.entities.Flights99Planes;
 import com.william.apirest.repositories.Flights99PlanesRepository;
+import com.william.apirest.service.ServiceFlights99Planes;
 
 import jakarta.validation.Valid;
 
@@ -30,56 +29,37 @@ public class Flights99PlanesController {
 	@Autowired
 	Flights99PlanesRepository flights99PlanesRepository;
 	
+	@Autowired
+	ServiceFlights99Planes service;
+	
 	@PostMapping("/99planes/newflight")
 	public ResponseEntity<Flights99Planes> saveFlights(@RequestBody @Valid Flights99PlanesRecordDto flights99PlanesRecordDto){
-		var flights99Planes = new Flights99Planes();
-		BeanUtils.copyProperties(flights99PlanesRecordDto, flights99Planes);
-		return ResponseEntity.status(HttpStatus.CREATED).body(flights99PlanesRepository.save(flights99Planes));
+		Flights99Planes flights99Planes = service.saveFlights(flights99PlanesRecordDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(flights99Planes);
 	}
 	
 	@GetMapping("/99planes/allflights")
 	public ResponseEntity<List<Flights99Planes>> getAllFlights(){
-		return ResponseEntity.status(HttpStatus.OK).body(flights99PlanesRepository.findAll());
+		return ResponseEntity.status(HttpStatus.OK).body(service.getAllFlights());
 	}
 	
 	@GetMapping("/99planes/oneflight/{id}")
 	public ResponseEntity<Object> getOneFlight(@PathVariable(value="id") UUID id){
-		Optional<Flights99Planes> flight = flights99PlanesRepository.findById(id);
-		if(flight.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Flight not found.");
-		}
-		return ResponseEntity.status(HttpStatus.OK).body(flight.get());
+		return ResponseEntity.status(HttpStatus.OK).body(service.getOneFlight(id));
 	}
 	
 	@PutMapping("/99planes/flight/{id}")
 	public ResponseEntity<Object> updateFlight(@PathVariable(value="id") UUID id, @RequestBody @Valid Flights99PlanesRecordDto flights99PlanesRecordDto){
-		Optional<Flights99Planes> flight = flights99PlanesRepository.findById(id);
-		if(flight.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Flight not found.");
-		}
-		var Flights99Planes = flight.get();
-		BeanUtils.copyProperties(flights99PlanesRecordDto, Flights99Planes);
-		return ResponseEntity.status(HttpStatus.OK).body(flights99PlanesRepository.save(Flights99Planes));
+		return ResponseEntity.status(HttpStatus.OK).body(service.updateFlight(id, flights99PlanesRecordDto));
 	}
 	
 	@PatchMapping("/99planes/flight/{id}")
-	public ResponseEntity<Object> updateFlightPach(@PathVariable(value="id") UUID id, @RequestBody @Valid Flights99PlanesPatchRecordDto flights99PlanesPatchRecordDto ){
-		Optional<Flights99Planes> flight = flights99PlanesRepository.findById(id);
-		if(flight.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Flight not found.");
-		}
-		var Flights99Planes = flight.get();
-		BeanUtils.copyProperties(flights99PlanesPatchRecordDto, Flights99Planes);
-		return ResponseEntity.status(HttpStatus.OK).body(flights99PlanesRepository.save(Flights99Planes));
+	public ResponseEntity<Object> updateFlightPatch(@PathVariable(value="id") UUID id, @RequestBody @Valid Flights99PlanesPatchRecordDto flights99PlanesPatchRecordDto ){
+		return ResponseEntity.status(HttpStatus.OK).body(service.updateFlightPatch(id, flights99PlanesPatchRecordDto));
 	}
 	
 	@DeleteMapping("/99planes/flight/{id}")
 	public ResponseEntity<Object> deleteFlight(@PathVariable(value="id") UUID id){
-		Optional<Flights99Planes> flight = flights99PlanesRepository.findById(id);
-		if(flight.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Flight not found.");
-		}
-		flights99PlanesRepository.delete(flight.get());
-		return ResponseEntity.status(HttpStatus.OK).body("Flight Successfully Deleted.");
+		return ResponseEntity.status(HttpStatus.OK).body(service.deleteFlight(id));
 	}
 }
---
+++ src/main/java/com/william/apirest/service/ServiceFlights99Planes.java
@@ -0,0 +1,69 @@
+package com.william.apirest.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.william.apirest.dtos.Flights99PlanesPatchRecordDto;
+import com.william.apirest.dtos.Flights99PlanesRecordDto;
+import com.william.apirest.entities.Flights99Planes;
+import com.william.apirest.repositories.Flights99PlanesRepository;
+
+@Service
+public class ServiceFlights99Planes {
+	
+	@Autowired
+	private Flights99PlanesRepository flights99PlanesRepository;
+
+	public Flights99Planes saveFlights(Flights99PlanesRecordDto flights99PlanesRecordDto) {
+		Flights99Planes flights99Planes = new Flights99Planes();
+		BeanUtils.copyProperties(flights99PlanesRecordDto, flights99Planes);
+		return flights99PlanesRepository.save(flights99Planes);
+	}
+	
+	public List<Flights99Planes> getAllFlights(){
+		return flights99PlanesRepository.findAll();
+	}
+	
+	public Optional<Flights99Planes> getOneFlight(@PathVariable(value="id") UUID id){
+		return flights99PlanesRepository.findById(id);
+	}
+	
+	public Object updateFlight(@PathVariable(value="id") UUID id, Flights99PlanesRecordDto flights99PlanesRecordDto){
+		Optional<Flights99Planes> flights99Planes = flights99PlanesRepository.findById(id);
+		if(flights99Planes.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Flight not found.");
+		}
+		var Flights99Planes = flights99Planes.get();
+		BeanUtils.copyProperties(flights99PlanesRecordDto, Flights99Planes);
+		return flights99PlanesRepository.save(Flights99Planes);
+		
+	}
+	
+	public Object updateFlightPatch(@PathVariable(value="id") UUID id, Flights99PlanesPatchRecordDto flights99PlanesPatchRecordDto){
+		Optional<Flights99Planes> flights99Planes = flights99PlanesRepository.findById(id);
+		if(flights99Planes.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Flight not found.");
+		}
+		var Flights99Planes = flights99Planes.get();
+		BeanUtils.copyProperties(flights99PlanesPatchRecordDto, Flights99Planes);
+		return flights99PlanesRepository.save(Flights99Planes);
+		
+	}
+	
+	public Object deleteFlight(@PathVariable(value="id") UUID id){
+		Optional<Flights99Planes> flights99Planes = flights99PlanesRepository.findById(id);
+		if(flights99Planes.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Flight not found.");
+		}
+		flights99PlanesRepository.delete(flights99Planes.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Flight Successfully Deleted.");
+	}
+}
